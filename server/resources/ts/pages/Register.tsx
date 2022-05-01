c0fce2d34f548695e3ab764cc266e62cd619ed54
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-05-01 20:44:47 +0900

    18 Register

--- server/resources/ts/pages/Register.tsx
+++ server/resources/ts/pages/Register.tsx
@@ -1,19 +1,51 @@
-import React from "react";
+import React, { SyntheticEvent, useState } from "react";
+import axios from "axios";
 
 const Register = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+
+  const submit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    const response = await axios.post("http://localhost/api/register", {
+      first_name: firstName,
+      last_name: lastName,
+      email: email,
+      password: password,
+      password_confirm: passwordConfirm
+    });
+
+    console.log(response);
+  };
+
   return (
-    <form className="form-signin">
+    <form className="form-signin" onSubmit={submit}>
       <h1 className="h3 mb-3 font-weight-normal">Please register</h1>
 
-      <input className="form-control mb-2" placeholder="First Name" required />
+      <input
+        className="form-control mb-2"
+        placeholder="First Name"
+        required
+        onChange={e => setFirstName(e.target.value)}
+      />
 
-      <input className="form-control mb-2" placeholder="Last Name" required />
+      <input
+        className="form-control mb-2"
+        placeholder="Last Name"
+        required
+        onChange={e => setLastName(e.target.value)}
+      />
 
       <input
         type="email"
         className="form-control mb-2"
         placeholder="Email"
         required
+        onChange={e => setEmail(e.target.value)}
       />
 
       <input
@@ -21,6 +53,7 @@ const Register = () => {
         className="form-control mb-2"
         placeholder="Password"
         required
+        onChange={e => setPassword(e.target.value)}
       />
 
       <input
@@ -28,10 +61,11 @@ const Register = () => {
         className="form-control mb-2"
         placeholder="Password Confirm"
         required
+        onChange={e => setPasswordConfirm(e.target.value)}
       />
 
       <button className="btn btn-lg btn-primary btn-block w-100" type="submit">
-        Regitster
+        Register
       </button>
     </form>
   );

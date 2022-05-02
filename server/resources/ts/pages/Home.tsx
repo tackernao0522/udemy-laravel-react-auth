b6367e1628617076f1914b3b615b587809d30d19
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-05-02 12:25:12 +0900

    21 Authenticated User

--- server/resources/ts/pages/Home.tsx
+++ server/resources/ts/pages/Home.tsx
@@ -1,9 +1,29 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get("user");
+
+        // console.log(response);
+        const user = response.data;
+
+        setMessage(`Hi ${user.first_name} ${user.last_name}`);
+      } catch (e) {
+        setMessage("You are not logged in!");
+      }
+    })();
+  }, []);
+
   return (
     <div className="container">
-      <h1>You are not logged in!</h1>
+      <h1>
+        {message}
+      </h1>
     </div>
   );
 };
